87508e933a7b5f258290ee65c359e6135feadd31
Chris-techz <77393492+Chris-techz@users.noreply.github.com>
2021-11-27 13:24:22 +0100

    Update reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -4,7 +4,6 @@
 //Replacement of the STL (and others includes)
 #include "mystl.h"
 
-// COUCOU marine, voici mon code modifié
 class Glossary : public MyVector
 {
 private:
@@ -36,7 +35,7 @@ static void _NewWordFound(Glossary& _glossary, std::ofstream& _reverseFile, MySt
     _word.clear();
 }
 
-static std::vector<char> gRealCharacters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'é', 'è', 'ê', 'à', 'â', 'ë', 'û', 'ù', 'î', 'ç', 'ô', 'ö', 'ü' };
+static std::vector<char> gRealCharacters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Ã©', 'Ã¨', 'Ãª', 'Ã ', 'Ã¢', 'Ã«', 'Ã»', 'Ã¹', 'Ã®', 'Ã§', 'Ã´', 'Ã¶', 'Ã¼' };
 bool _IsRealCharacter(const char _char) { return (std::cend(gRealCharacters) != std::find(std::cbegin(gRealCharacters), std::cend(gRealCharacters), _char)); }
 
 #define CHECKWORDLENGTH                                                         \
